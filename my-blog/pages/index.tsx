c4967e16f295e328799881bfa778500e6e9f7957
rashmipbhat <rashmipbhat97@gmail.com>
2022-10-07 12:32:45 +0530

    Next.js initial set up

---
+++ my-blog/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+     <p style={{color:'white',fontFamily:'cursive'}}>Hey there!</p> 
+    </div>
+  )
+}
+
+export default Home
